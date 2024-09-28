05c34014eca180c1f008c8b93e200c591060d101
Douglas Faria <hfsdouglas@gmail.com>
2024-09-27 23:20:35 -0300

    feat: add sidebar component and syles

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,26 @@
+import styles from './sidebar.module.css'
+import { PencilLine } from '@phosphor-icons/react'
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1517852058149-07c7a2e65cc6?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            />
+            
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/hfsdouglas.png" />
+
+                <strong>Douglas Faria</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
