b2cf1ecaf4536bfd4f6d65c1a8f1e0e2b6aa97bc
Douglas Faria <hfsdouglas@gmail.com>
2024-09-30 20:55:11 -0300

    feat: array of posts

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import { Sidebar } from './components/sidebar';
 
 import styles from './app.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/hfsdouglas.png',
+      name: 'Douglas Faria',
+      role: 'Web Developer'
+    },
+    contents: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2024-09-29 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'Web Developer'
+    },
+    contents: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2024-09-30 20:00:00'),
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -15,8 +46,16 @@ export function App() {
         <Sidebar />
         
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.contents}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
