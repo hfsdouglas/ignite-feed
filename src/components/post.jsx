ecace2d48f20542ccc8d10925d36997a38920b29
Douglas Faria <hfsdouglas@gmail.com>
2024-09-30 21:00:50 -0300

    feat: add rendering content data to the post

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -32,17 +32,13 @@ export function Post({ author, contents, publishedAt }) {
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-                <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
-
-                <p>
-                    <a href="">#novoprojeto</a>{' '}
-                    <a href="">#nlw</a>{' '}
-                    <a href="">#rocketseat</a>{' '}
-                </p>
+                {contents.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if (line.type === 'link') {
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
             </div>
 
             <form className={styles.commentForm}>
